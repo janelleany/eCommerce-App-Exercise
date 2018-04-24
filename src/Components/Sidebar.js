3d9240e276341865ad5c9a7ecd4155a759a1e395
janelleany <janellealexander12@gmail.com>
2018-04-24 17:47:42 -0400

    feeding Sidebar the array of categories
    in the middle of creating .map to render sidebar with all categories

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import '../App.css';
 
+import { Link } from "react-router-dom";
+
+import { connect } from 'react-redux';
+
+let mapStateToProps = (state) => {
+  return {categories: state.categoriesKey}
+}
 
 
-let Sidebar = () => {
+let Sidebar = ({categories}) => {
   return (
     <div>
-      Sidebar has rendered
+      <div><Link to="/">Etsetera...</Link></div>
+      <div>
+        {
+          categories.map()
+        }
+      </div>
     </div>
   )
 }
